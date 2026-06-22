5c6171209b8c614a46bcad8d38902e7f1fdf775e
thekhaleed <brian.m2129@gmail.com>
2026-06-22 07:39:37 +0300

    initial commit

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,3 +1,8 @@
+/**
+ * This program displays a greeting
+ * @version 1.0 2026-June-22
+ * @author thekhaleed
+*/
 public class Hello {
     public static void main(String[]args){
         System.out.println("Hello Mr. Mwangi, welcome back to the Java language!");
